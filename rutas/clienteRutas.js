4ce2554e21b6d81e2b153a4fb24dd88abc3d2869
Maime03 <79386225+Maime03@users.noreply.github.com>
2024-05-13 19:11:34 -0400

    Add files via upload
    se realizo las diferentes consultas en mongo

--- rutas/clienteRutas.js
+++ rutas/clienteRutas.js
@@ -19,7 +19,7 @@ rutas.post('/crear', async (req, res) => {
         nombre: req.body.nombre,
         apellidos: req.body.apellidos,
         telefono: req.body.telefono,
-        tdireccion: req.body.direccion,
+        direccion: req.body.direccion,
         c_i: req.body.c_i
     })
     try {
@@ -55,5 +55,61 @@ rutas.delete('/eliminar/:id',async (req, res) => {
        } 
     catch (error) {
         res.status(500).json({ mensaje :  error.message})
+    }
+});
+
+// - 1. Esta consulta busca todos los clientes cuyo nombre sea "Naye".
+rutas.get('/busEspefifico', async (req, res) => {
+    try {
+        const clientesConNombreNaye = await ClienteModel.find({ nombre: "naye" });
+       res.status(200).json(clientesConNombreNaye);
+    } catch(error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
+
+// - 2.  Esta consulta utilizará la agregación para calcular el número total de clientes con el que contamos.
+rutas.get('/totalCliente', async (req, res) => {
+    try {
+        const totalClientes = await ClienteModel.aggregate([{ $group: { _id: null, count: { $sum: 1 } } } ]);
+       res.status(200).json(totalClientes);
+    } catch(error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
+// - 3.  Esta consulta buscará a los clientes cuyos nombres comiencen con la letra "S", y no importar que sea en mayúsculas o minúsculas.
+rutas.get('/busLetra', async (req, res) => {
+    try {
+        const clientesNombreEmpieceConS = await ClienteModel.find({ nombre: { $regex: /^S/i } });
+       res.status(200).json(clientesNombreEmpieceConS);
+    } catch(error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
+// - 4.  Esta consulta de agregación para contar el número de clientes por ciudad
+rutas.get('/conteoClientesPorCiudad', async (req, res) => {
+    try {
+        const conteo = await ClienteModel.aggregate([{ $group: { _id: "$direccion", total: { $sum: 1 } } }]);
+        res.json(conteo);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
+// - 5.  Esta consulta de clientes que vivan en una dirección específica
+rutas.get('/clientesPorDireccion/:direccion', async (req, res) => {
+    try {
+        const clientes = await ClienteModel.find({ direccion: req.params.direccion });
+        res.json(clientes);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
+    }
+});
+// - 6.  Esta consulta de Ordenamiento por nombre y luego por apellidos de forma descendente
+rutas.get('/clientesOrdenadosPorNombreYApellidos', async (req, res) => {
+    try {
+        const clientes = await ClienteModel.find().sort({ nombre: -1, apellidos: -1 });
+        res.json(clientes);
+    } catch (error) {
+        res.status(500).json({ mensaje: error.message });
     }
 });
